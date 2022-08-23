c09336f806ec4a63179c36a9c35727e21334af43
Fahim1272 <imfahim72@gmail.com>
2022-08-23 12:08:46 +0600

    calculator-function added

--- js/calculator.js
+++ js/calculator.js
@@ -1,15 +1,36 @@
+
+// First Button
+// Step:1
 document.getElementById('player-cost-btn').addEventListener('click', function(){
     // step:1 Get the player cost from input field.
     const playerCost= document.getElementById('player-cost');
-    const playervalue = playerCost.value;
+    let playervalue = playerCost.value;
     // console.log(playervalue);
 
     // step:2 Set The total cost in the total player expences segment:
 
     const previousValue = document.getElementById('allplayer-expences');
-    const abcd = previousValue.innerText;
+    // const abcd = previousValue.innerText;
     // console.log(abcd);
     previousValue.innerText = playervalue;
     // console.log(playervalue);
 });
-document.getElementById
+
+// Second-Button
+// Step-1:
+document.getElementById('all-expences-btn').addEventListener('click',function(){
+    // step:2
+    const managerCost = document.getElementById('manager-cost');
+    const managerStringValue = managerCost.value;
+    const managerValue =parseFloat(managerStringValue);
+    // step:3
+    const coachCost = document.getElementById('coach-cost');
+    const coachStringValue = coachCost.value;
+    const coachValue =parseFloat(coachStringValue);
+    // Step:4
+    const totalExpences = managerValue + coachValue;
+
+    const totalExpencesString = document.getElementById('all-expences');
+    // const dbcs =  totalExpencesString.innerText;
+    totalExpencesString.innerText = totalExpences;
+})
